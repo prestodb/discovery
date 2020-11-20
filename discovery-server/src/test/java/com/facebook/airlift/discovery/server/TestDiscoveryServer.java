0038be346c2b4fe3668e5ec2c23e274755ba898a
Leiqing Cai <caithagoras@gmail.com>
2020-11-19 19:07:37 -0800

    Update to Airlift 0.198 and Airbase 100

--- discovery-server/src/test/java/com/facebook/airlift/discovery/server/TestDiscoveryServer.java
+++ discovery-server/src/test/java/com/facebook/airlift/discovery/server/TestDiscoveryServer.java
@@ -105,7 +105,6 @@ public class TestDiscoveryServer
                 });
 
         Injector serverInjector = bootstrap
-                .strictConfig()
                 .doNotInitializeLogging()
                 .setRequiredConfigurationProperties(serverProperties)
                 .initialize();
@@ -137,7 +136,6 @@ public class TestDiscoveryServer
                 new DiscoveryModule());
 
         Injector announcerInjector = bootstrap
-                .strictConfig()
                 .doNotInitializeLogging()
                 .setRequiredConfigurationProperties(announcerProperties)
                 .initialize();
@@ -235,7 +233,6 @@ public class TestDiscoveryServer
                 });
 
         Injector clientInjector = bootstrap
-                .strictConfig()
                 .doNotInitializeLogging()
                 .setRequiredConfigurationProperties(clientProperties)
                 .initialize();
